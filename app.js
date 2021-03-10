126ed1b3d9dc7274ed09f33ffa25e6d9785feb62
Siddh <sid.singh9099@gmail.com>
2021-03-10 20:59:42 +0530

    After complete Second Art.

--- app.js
+++ app.js
@@ -56,6 +56,16 @@ var art9 = document.querySelector(".art9");
 var art10 = document.querySelector(".art10");
 var firstart = document.querySelector(".firstart");
 
+
+var secondart1 = document.querySelector(".secondart1");
+var secondart2 = document.querySelector(".secondart2");
+var secondart3 = document.querySelector(".secondart3");
+var secondart4 = document.querySelector(".secondart4");
+var secondart5 = document.querySelector(".secondart5");
+var secondart6 = document.querySelector(".secondart6");
+var secondart7 = document.querySelector(".secondart7");
+
+
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var color3 = document.querySelector(".color3");
@@ -65,6 +75,10 @@ var color6 = document.querySelector(".color6");
 var shape2 = document.querySelector(".shape2");
 
 var array =[art0,art1,art2,art3,art4,art5,art6,art7,art8,art9,art10,];
+var array1 = [secondart1,secondart2,secondart3,secondart4,secondart5,secondart6,secondart7];
+ 
+// for frist art
+
 
 firstart.addEventListener(`click`,()=>{
     for (let index = 0; index < array.length; index++) {
@@ -87,8 +101,59 @@ firstart.addEventListener(`click`,()=>{
             element.style.backgroundColor = "#404040";
         }
         
-    }
-})
+    };
+
+    for (let index = 0; index < array1.length; index++) {
+        const element = array1[index];
+            element.style.border = "#404040";
+            element.style.borderStyle = "solid";
+            if (element == secondart1) {
+                element.style.width = "0.1cm"
+                element.style.height = "0.3cm"
+                element.style.bottom = "15cm"
+                element.style.left = "8cm"
+
+            } else if(element == secondart2){
+                element.style.width = "0.1cm"
+                element.style.height = "0.3cm"
+                element.style.bottom = "14.4cm"
+                element.style.left = "8.5cm"
+
+            } else if(element == secondart3){
+                element.style.width ="0.1cm"
+                element.style.height ="0.3cm"
+                element.style.bottom ="15.8cm"
+                element.style.left = "9cm" 
+            }
+             else if(element == secondart4){
+                element.style.width = "0.1cm"
+                element.style.height = "0.3cm"
+                element.style.bottom = "15.3cm"
+                element.style.left = "9.5cm" 
+            }
+             else if(element == secondart5){
+                element.style.width = "4cm"
+                element.style.height = "2cm"
+                element.style.bottom = "38.4cm"
+                element.style.left = "7.8cm" 
+            }
+             else if(element == secondart6){
+                element.style.width = "0.8cm"
+                element.style.height = "0.8cm"
+                element.style.bottom = "38cm"
+                element.style.left = "9.4cm" 
+            }
+            else if(element == secondart7){
+                element.style.width = "1.5cm"
+                element.style.height = "1.5cm"
+                element.style.bottom = "43.6cm"
+                element.style.left = "9.1cm" 
+            }
+        }
+});
+
+
+// for second art
 
 shape2.addEventListener(`click`,()=>{
     for (let index = 0; index < array.length; index++) {
@@ -107,7 +172,80 @@ shape2.addEventListener(`click`,()=>{
                 element.style.backgroundColor = "#404040";
                 element.style.border = "#404040";   
         }
+    };
+
+    for (let index = 0; index < array1.length; index++) {
+        const element = array1[index];
+            element.style.border = "white";
+            element.style.borderStyle = "solid";
+            element.style.backgroundColor = "#404040";
+            if (element == secondart1) {
+                element.style.width = "0.5cm"
+                element.style.height = "4cm"
+                element.style.bottom = "18cm"
+                element.style.left = "8cm"
+
+            } else if(element == secondart2){
+                element.style.width = "0.5cm"
+                element.style.height = "4cm"
+                element.style.bottom = "22.12cm"
+                element.style.left = "9cm"
+
+            } else if(element == secondart3){
+                element.style.width ="0.5cm"
+                element.style.height ="4cm"
+                element.style.bottom ="26.26cm"
+                element.style.left = "10cm" 
+            }
+             else if(element == secondart4){
+                element.style.width = "0.5cm"
+                element.style.height = "4cm"
+                element.style.bottom = "30.38cm"
+                element.style.left = "11cm" 
+            }
+             else if(element == secondart5){
+                element.style.width = "4cm"
+                element.style.height = "2cm"
+                element.style.bottom = "38.4cm"
+                element.style.left = "7.8cm" 
+            }
+             else if(element == secondart6){
+                element.style.width = "0.8cm"
+                element.style.height = "0.8cm"
+                element.style.bottom = "38cm"
+                element.style.left = "9.4cm" 
+            }
+            else if(element == secondart7){
+                element.style.width = "1.5cm"
+                element.style.height = "1.5cm"
+                element.style.bottom = "43.6cm"
+                element.style.left = "9.1cm" 
+            }
     }
+});
+
+// for box selection
+ 
+secondart1.addEventListener(`click`,()=>{
+    colorfiller(secondart1)
+});
+secondart2.addEventListener(`click`,()=>{
+    colorfiller(secondart2)
+});
+secondart3.addEventListener(`click`,()=>{
+    colorfiller(secondart3)
+});
+secondart4.addEventListener(`click`,()=>{
+    colorfiller(secondart4)
+});
+secondart5.addEventListener(`click`,()=>{
+    colorfiller(secondart5)
+});
+secondart6.addEventListener(`click`,()=>{
+    colorfiller(secondart6)
+});
+secondart7.addEventListener(`click`,()=>{
+    colorfiller(secondart7)
 });
 
 art1.addEventListener(`click`,()=>{
@@ -141,6 +279,7 @@ art10.addEventListener(`click`,()=>{
     colorfiller(art10);
 });
 
+// function for color fill
 
 function colorfiller(params) {
     fillincolor = params;
