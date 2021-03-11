9dcc2bd566569fe31df9ad4e40b4944ecac803e1
Siddh <sid.singh9099@gmail.com>
2021-03-11 12:28:07 +0530

    clean comment code

--- app.js
+++ app.js
@@ -1,47 +1,3 @@
-// let box2 = document.querySelector(".box2");
-// let boxa = document.querySelector(".boxa");
-// let box1 = document.querySelector(".box1");
-// let boxb = document.querySelector(".boxb");
-// let body = document.querySelector(".body");
-// let canvas = document.querySelector(".cnv");
-// let selectbox;
-
-// canvas.width = window.innerWidth;
-// canvas.height = window.innerHeight;
-
-// console.log(window.innerWidth);
-// console.log(window.innerHeight);
-
-// function colorfiller(parameters) { 
-//       selectbox = parameters;
-//       boxa.addEventListener(`mouseenter`,()=>{
-//             selectbox.style.backgroundColor = window.getComputedStyle(boxa).backgroundColor;
-//       },);
-//       boxb.addEventListener(`mouseenter`,()=>{
-//             selectbox.style.backgroundColor = window.getComputedStyle(boxb).backgroundColor;
-//       },);      
-// }
-
-// box1.addEventListener(`click`,()=>{
-//       colorfiller(box1);
-// },);
-
-// box2.addEventListener(`click`,()=>{
-//       colorfiller(box2);
-// },);
-
-//  var shape = canvas.getContext("2d");
-
-//  for (let index = 0; index < 3; index++) {
-//        let x = Math.random()*window.innerWidth;
-//        let y = Math.random()*window.innerHeight;
-//       //  shape.fillRect(x, y, 50, 50); 
-//       shape.beginPath();
-//       shape.arc(x, y, 20, 0, 2*Math.PI);
-//       shape.stroke();   
-//  }
-
-
 var fillincolor;
 var art0 = document.querySelector(".art0");
 var art1 = document.querySelector(".art1");
@@ -379,6 +335,3 @@ function colorfiller(params) {
         fillincolor.style.backgroundColor = window.getComputedStyle(color6).backgroundColor; 
     });
 };
-
-
-
