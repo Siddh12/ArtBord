2462a1631dc73dd33b2ce11dbc94c8d40d34508c
Siddh <sid.singh9099@gmail.com>
2021-03-17 19:27:19 +0530

    Apply class-id logic

--- app.js
+++ app.js
@@ -1,126 +1,30 @@
+
+const art1Array = ["arta","artb","artc","artd","arte","artf","artg","arth","arti","artj","artk"];
+let manupulatearray1 = art1Array.map(id=>document.getElementById(id));
+manupulatearray1.forEach(element=>element.addEventListener(`click`,e=>colorfiller(e.target)));
+
+const art2array = ["secondarta","secondartb","secondartc","secondartd","secondarte","secondartf","secondartg"];
+let manupulatearray2 = art2array.map(id=>document.getElementById(id));
+manupulatearray2.forEach((element)=>console.log(element));
+// element.addEventListener(`click`,(e)=> colorfiller(e.target)));
+
 var fillincolor;
-
-const art1Array = [".art0",".art1",".art2",".art3",".art4",".art5",".art6",".art7",".art8",".art9",".art10"];
-let manupulatearray1 = art1Array.map(id=>document.querySelector(id));
-manupulatearray1.forEach((element)=>element.addEventListener(`click`,(e)=> colorfiller(e.target)));
-
-const art2array = [".secondart1",".secondart2",".secondart3",".secondart4",".secondart5",".secondart6",".secondart7"];
-let manupulatearray2 = art2array.map(id=>document.querySelector(id));
-manupulatearray2.forEach((element)=>element.addEventListener(`click`,(e)=> colorfiller(e.target)));
-
 var firstart = document.querySelector(".firstart");
 var shape2 = document.querySelector(".shape2");
-
+const classoffirstart = ["art0","art1","art2","art3","art4","art5","art6","art7","art8","art9","art10"];
+const classofsecondart = ["secondart1","secondart2","secondart3","secondart4","secondart5","secondart6","secondart7"]; 
 
 // for frist art
 
-
 firstart.addEventListener(`click`,()=>{
-    for (let index = 0; index < manupulatearray1.length; index++) {
-        const element = manupulatearray1[index];
-        element.style.border= "white";
-        element.style.borderStyle = "solid";
-        element.style.backgroundColor = "#404040";
-        if (element == manupulatearray1[1]) {
-            element.style.width = "1.3cm";
-            element.style.height= "1.3cm";
-            element.style.bottom = "5.2cm";
-            element.style.left= "1.04cm";
-        } else if(element == manupulatearray1[2]){
-            element.style.width = "1.3cm";
-            element.style.height= "1.3cm";
-            element.style.bottom = "6.63cm";
-            element.style.left= "17.46cm";
-        } else if(element == manupulatearray1[3]){
-            element.style.width = "7cm";
-            element.style.height= "1cm";
-            element.style.bottom = "2.3cm";
-            element.style.left= "8cm";
-        } else if(element == manupulatearray1[4]){
-            element.style.width = "4cm";
-            element.style.height= "0.4cm";
-            element.style.bottom = "4.2cm";
-            element.style.left= "9.53cm";
-        } else if(element == manupulatearray1[5]){
-            element.style.width = "0.5cm";
-            element.style.height= "4cm";
-            element.style.bottom = "10cm";
-            element.style.left= "9.5cm";
-        } else if(element == manupulatearray1[6]){
-            element.style.width = "1.5cm";
-            element.style.height= "1.5cm";
-            element.style.bottom = "11cm";
-            element.style.left= "9.75cm";
-        } else if(element == manupulatearray1[7]){
-            element.style.width = "2cm";
-            element.style.height= "2cm";
-            element.style.bottom = "16.5cm";
-            element.style.left= "5cm";
-        } else if(element == manupulatearray1[8]){
-            element.style.width = "2cm";
-            element.style.height= "2cm";
-            element.style.bottom = "18.6cm";
-            element.style.left= "12.5cm";
-        } else if(element == manupulatearray1[9]){
-            element.style.width = "1.5cm";
-            element.style.height= "1.5cm";
-            element.style.bottom = "23.7cm";
-            element.style.left= "10.4cm";
-        } else if (element == manupulatearray1[10]) {
-            element.style.width = "1.5cm";
-            element.style.height= "1.5cm";
-            element.style.bottom = "25.32cm";
-            element.style.left= "7.5cm";
-        }   
-    };
-
     for (let index = 0; index < manupulatearray2.length; index++) {
         const element = manupulatearray2[index];
-            element.style.border = "#404040";
-            element.style.borderStyle = "solid";
-            if (element == manupulatearray2[0]) {
-                element.style.width = "1.5cm"
-                element.style.height = "1.5cm"
-                element.style.bottom = "35cm"
-                element.style.left = "8cm"
-
-            } else if(element == manupulatearray2[1]){
-                element.style.width = "0.1cm"
-                element.style.height = "0.3cm"
-                element.style.bottom = "44.4cm"
-                element.style.left = "8.5cm"
-
-            } else if(element == manupulatearray2[2]){
-                element.style.width ="0.1cm"
-                element.style.height ="0.3cm"
-                element.style.bottom ="45.8cm"
-                element.style.left = "9cm" 
-            }
-             else if(element == manupulatearray2[3]){
-                element.style.width = "0.1cm"
-                element.style.height = "0.3cm"
-                element.style.bottom = "45.3cm"
-                element.style.left = "9.5cm" 
-            }
-             else if(element == manupulatearray2[4]){
-                element.style.width = "4cm"
-                element.style.height = "2cm"
-                element.style.bottom = "38.4cm"
-                element.style.left = "7.8cm" 
-            }
-             else if(element == manupulatearray2[5]){
-                element.style.width = "0.8cm"
-                element.style.height = "0.8cm"
-                element.style.bottom = "48cm"
-                element.style.left = "9.4cm" 
-            }
-            else if(element == manupulatearray2[6]){
-                element.style.width = "1.5cm"
-                element.style.height = "1.5cm"
-                element.style.bottom = "43.6cm"
-                element.style.left = "9.1cm" 
-            }
-        }
+        element.classList.remove(classofsecondart[index]);
+    };
+    for (let index = 0; index < manupulatearray1.length; index++) {
+        const element = manupulatearray1[index];
+        element.classList.add(classoffirstart[index]);
+    }; 
 });
 
 
@@ -129,116 +33,12 @@ firstart.addEventListener(`click`,()=>{
 shape2.addEventListener(`click`,()=>{
     for (let index = 0; index < manupulatearray1.length; index++) {
         const element = manupulatearray1[index];
-        element.style.border= "#404040";
-        element.style.borderStyle = "solid";
-        element.style.backgroundColor = "#404040";
-        if (element == manupulatearray1[0]) {
-            element.style.width = "0cm";
-            element.style.height= "0.2cm";
-            element.style.bottom = "55.2cm";
-            element.style.left= "1.04cm";
-        } else if (element == manupulatearray1[1]) {
-            element.style.width = "0.2cm";
-            element.style.height= "0.2cm";
-            element.style.bottom = "15.2cm";
-            element.style.left= "1.04cm";
-        } else if(element == manupulatearray1[2]){
-            element.style.width = "0.2cm";
-            element.style.height= "0.2cm";
-            element.style.bottom = "26.63cm";
-            element.style.left= "17.46cm";
-        } else if(element == manupulatearray1[3]){
-            element.style.width = "0.2cm";
-            element.style.height= "0.1cm";
-            element.style.bottom = "22.3cm";
-            element.style.left= "8cm";
-        } else if(element == manupulatearray1[4]){
-            element.style.width = "0.4cm";
-            element.style.height= "0.1cm";
-            element.style.bottom = "24.2cm";
-            element.style.left= "9.53cm";
-        } else if(element == manupulatearray1[5]){
-            element.style.width = "0.4cm";
-            element.style.height= "0.1cm";
-            element.style.bottom = "30cm";
-            element.style.left= "9.5cm";
-        } else if(element == manupulatearray1[6]){
-            element.style.width = "0.1cm";
-            element.style.height= "0.1cm";
-            element.style.bottom = "31cm";
-            element.style.left= "9.75cm";
-        } else if(element == manupulatearray1[7]){
-            element.style.width = "0.2cm";
-            element.style.height= "0.2cm";
-            element.style.bottom = "26.5cm";
-            element.style.left= "5cm";
-        } else if(element == manupulatearray1[8]){
-            element.style.width = "0.2cm";
-            element.style.height= "0.2cm";
-            element.style.bottom = "28.6cm";
-            element.style.left= "12.5cm";
-        } else if(element == manupulatearray1[9]){
-            element.style.width = "0.1cm";
-            element.style.height= "0.1cm";
-            element.style.bottom = "23.7cm";
-            element.style.left= "10.4cm";
-        } else if (element == manupulatearray1[10]) {
-            element.style.width = "1.5cm";
-            element.style.height= "1.5cm";
-            element.style.bottom = "25.32cm";
-            element.style.left= "7.5cm";
-        }
-    };
-
+        element.classList.remove(classoffirstart[index]);
+    }; 
     for (let index = 0; index < manupulatearray2.length; index++) {
         const element = manupulatearray2[index];
-            element.style.border = "white";
-            element.style.borderStyle = "solid";
-            element.style.backgroundColor = "#404040";
-
-            if (element == manupulatearray2[0]) {
-                element.style.width = "0.5cm"
-                element.style.height = "4cm"
-                element.style.bottom = "6cm"
-                element.style.left = "8.5cm"
-
-            } else if(element == manupulatearray2[1]){
-                element.style.width = "0.5cm"
-                element.style.height = "4cm"
-                element.style.bottom = "10.12cm"
-                element.style.left = "9.5cm"
-
-            } else if(element == manupulatearray2[2]){
-                element.style.width ="0.5cm"
-                element.style.height ="4cm"
-                element.style.bottom ="14.26cm"
-                element.style.left = "10.5cm" 
-            }
-             else if(element == manupulatearray2[3]){
-                element.style.width = "0.5cm"
-                element.style.height = "4cm"
-                element.style.bottom = "18.38cm"
-                element.style.left = "11.5cm" 
-            }
-             if(element == manupulatearray2[4]){
-                element.style.width = "4cm"
-                element.style.height = "2cm"
-                element.style.bottom = "26.4cm"
-                element.style.left = "8.25cm" 
-            }
-             else if(element == manupulatearray2[5]){
-                element.style.width = "0.8cm"
-                element.style.height = "0.8cm"
-                element.style.bottom = "26cm"
-                element.style.left = "9.85cm" 
-            }
-            else if(element == manupulatearray2[6]){
-                element.style.width = "1.5cm"
-                element.style.height = "1.5cm"
-                element.style.bottom = "31.6cm"
-                element.style.left = "9.5cm" 
-            }
-    }
+        element.classList.add(classofsecondart[index]);
+    };
 });
 
 
@@ -325,27 +125,45 @@ lock6.addEventListener(`click`,()=>{
 });
 var condition =[true, true, true, true, true,true ];
 
-var color7 = document.querySelector(".color7")
+var color7 = document.querySelector(".color7");
+color7.onmousedown = moreclick;
+color7.onmouseup = onclickrelise;
 
-color7.addEventListener(`click`,()=>{
-    moreclick(color7); 
-   for (let index = 0; index < colorarraymanupulate.length; index++) {
-       const element = colorarraymanupulate[index];
-       if (condition[index]) {
-        var colorcode1 = Math.round(856635*Math.random());
-        element.style.backgroundColor = "#"+colorcode1.toString();
-       }
-       } ;  
-});
+function moreclick() {
+    color7.style.backgroundColor= "#3A5444";
+    color7.style.color= "white";
+};
+function onclickrelise(){  
+    color7.style.backgroundColor= "White";
+    color7.style.color= "Black";
+    for (let index = 0; index < colorarraymanupulate.length; index++) {
+               const element = colorarraymanupulate[index];
+               if (condition[index]) {
+                var colorcode1 = Math.round(856635*Math.random());
+                element.style.backgroundColor = "#"+colorcode1.toString();
+               }
+               } ; 
+};
 
-// click Animation
+// color7.addEventListener(`click`,()=>{
+//     moreclick(color7); 
+//    for (let index = 0; index < colorarraymanupulate.length; index++) {
+//        const element = colorarraymanupulate[index];
+//        if (condition[index]) {
+//         var colorcode1 = Math.round(856635*Math.random());
+//         element.style.backgroundColor = "#"+colorcode1.toString();
+//        }
+//        } ;  
+// });
 
- function moreclick(element){
-        element.style.backgroundColor= "#3A5444";
-        element.style.color= "white";
-   setTimeout(()=>{
-        element.style.backgroundColor= "White";
-        element.style.color= "Black";
-   },150)  
-   };
+// // click Animation
+
+//  function moreclick(element){
+//         element.style.backgroundColor= "#3A5444";
+//         element.style.color= "white";
+//    setTimeout(()=>{
+//         element.style.backgroundColor= "White";
+//         element.style.color= "Black";
+//    },150)  
+//    };
    
